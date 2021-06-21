fc2a2dd5eae0db1c00613e07cc39636c6b221ab8
kusigeri <kusi@vipmail.hu>
2021-06-21 19:35:56 +0200

    Kész az osztalyokhoz a java file

---
+++ Battleship.java
@@ -0,0 +1,10 @@
+public class Battleship {
+    public static void main(String[] args) {
+        /*
+        Az osztálynak Battleshipvannak olyan mezői Display, Inputamelyeket az egész program használ.
+        Az osztálynak Battleshipvan egy hurka, amelyben a program fut.
+        Az osztály Battleshipmegmutatja a főmenüt, és lehetővé teszi a felhasználó számára,
+        hogy új játékot indítson, bemutassa a toplistákat és kilépjen.
+        */
+    }
+}
